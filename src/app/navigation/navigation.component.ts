d196a9fb07a1d12667ac61fa6f64209976604bbf
JordanBowker <jordanbowker90@gmail.com>
2019-04-22 10:08:56 +0100

    switched to materialize css

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , AfterViewInit} from '@angular/core';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    jQuery('.menu-dropdown-trigger').dropdown();
+  }
 }
