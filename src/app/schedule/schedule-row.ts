549d44706bba576086e69a24b767491b65e4fad2
Jordan Bowker <jordanbowker90@gmail.com>
2019-04-24 20:53:30 +0100

    Have changed the styling and updated the schedule

--- src/app/schedule/schedule-row.ts
+++ src/app/schedule/schedule-row.ts
@@ -1,19 +1,23 @@
 export class ScheduleRow {
-    time: string;
+    start: string;
+    finish: string;
     activity: string;
 
-    constructor(time: string, activity: string) {
-        this.time = time;
+    constructor(start: string, finish: string, activity: string) {
+        this.start = start;
+        this.finish = finish;
         this.activity = activity;
     }
 }
 
 export const SCHEDULE_ROWS = [
-    new ScheduleRow('9:45', 'Registration Opens'),
-    new ScheduleRow('10:15', 'First Classes: Golden Oldie, Puppy'),
-    new ScheduleRow('11:30', 'Something'),
-    new ScheduleRow('12:20', 'Something'),
-    new ScheduleRow('13:20', 'Something'),
-    new ScheduleRow('14:20', 'Something'),
-    new ScheduleRow('15:20', 'Something'),
+    new ScheduleRow('10:15', '-', 'Registration Opens'),
+    new ScheduleRow('10:50', '11:00', 'Shutzhund Introduction'),
+    new ScheduleRow('11:00', '11:45', 'Classes: Golden Oldies, Young Handlers 11-16ys, Young Handlers 6-10ys'),
+    new ScheduleRow('11:55', '12:10', 'Shutzhund Demonstration'),
+    new ScheduleRow('12:25', '13:25', 'Classes: Cheekiest Puppies, Best Rescue, Waggiest Tail, Best Trick'),
+    new ScheduleRow('13:30', '13:50', 'Terrier Racing Demonstration'),
+    new ScheduleRow('13:50', '14:35', 'Classes: Handsomest Dog, Prettiest Female, Dog The Judge Wants To Take Home'),
+    new ScheduleRow('14:40', '15:10', 'Terrier Racing - Open Demo'),
+    new ScheduleRow('15:10', '15:30', 'Terrier Racing - Finals')
 ];
